01b1b6208075c6cc587fc4e3e3a3746fb8923be5
Michael Shi <mfshi03@gmail.com>
2023-12-16 19:59:21 -0500

    adding inference code and parsing

---
+++ finetuning_openai.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+client = OpenAI()
+
+client.files.create(
+  file=open("mydata.jsonl", "rb"),
+  purpose="fine-tune"
+)
+
+client.fine_tuning.jobs.create(
+  training_file="file-abc123", 
+  model="gpt-3.5-turbo"
+)
+
+
+# List 10 fine-tuning jobs
+client.fine_tuning.jobs.list(limit=10)
+
+# Retrieve the state of a fine-tune
+client.fine_tuning.jobs.retrieve("ftjob-abc123")
+
+# Cancel a job
+client.fine_tuning.jobs.cancel("ftjob-abc123")
+
+# List up to 10 events from a fine-tuning job
+client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-abc123", limit=10)
+
+# Delete a fine-tuned model (must be an owner of the org the model was created in)
+client.models.delete("ft:gpt-3.5-turbo:acemeco:suffix:abc123")
+
+
+response = client.chat.completions.create(
+  model="ft:gpt-3.5-turbo:my-org:custom_suffix:id",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+print(completion.choices[0].message)
--- response.py
+++ response.py
@@ -1,29 +1,36 @@
 import os
 import openai 
+import re
+from dotenv import load_dotenv
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_KEY")
+
+pattern = re.compile(r"\`\`\`sysml([\s\S]*?)\`\`\`")
+
 prompts = [
-    "Generate SysMLv2 code for {}"
+    "Generate SysMLv2 code for {}",
+    "Convert the following SysMLv2 code to python: {}",
 ]
-def completion(code:str) -> str:
-    temp = str(search_terms)
+
+def completion(input:str, i:int=0) -> str:
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
             {
                 "role": "user",
-                "content": f"Convert the following SysMLv2 code to python {str}"
+                "content": prompts[0].format(input)
             },
         ],
         temperature=0,
-        max_tokens=1000,
-        top_p=1,
+        max_tokens=500,
+        top_p=0,
         frequency_penalty=0,
         presence_penalty=0
     )
+    return response.choices[0]["message"]["content"]
 
-    return response.choices[0].text
-
-
+text = completion("Rocket ship")
+print(re.findall(pattern, text)[0])
 
 
