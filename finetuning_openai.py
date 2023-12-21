28ddecb6fbd550cab5cc9368e8b4191a84fef878
Michael Shi <mfshi03@gmail.com>
2023-12-21 12:29:28 -0500

    Add terrain recommendations for UGV in different environments

--- finetuning_openai.py
+++ finetuning_openai.py
@@ -13,19 +13,20 @@ client.fine_tuning.jobs.create(
 
 
 # List 10 fine-tuning jobs
-client.fine_tuning.jobs.list(limit=10)
+def fine_tuning_funcs():
+  client.fine_tuning.jobs.list(limit=10)
 
-# Retrieve the state of a fine-tune
-client.fine_tuning.jobs.retrieve("ftjob-abc123")
+  # Retrieve the state of a fine-tune
+  client.fine_tuning.jobs.retrieve("ftjob-abc123")
 
-# Cancel a job
-client.fine_tuning.jobs.cancel("ftjob-abc123")
+  # Cancel a job
+  client.fine_tuning.jobs.cancel("ftjob-abc123")
 
-# List up to 10 events from a fine-tuning job
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-abc123", limit=10)
+  # List up to 10 events from a fine-tuning job
+  client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-abc123", limit=10)
 
-# Delete a fine-tuned model (must be an owner of the org the model was created in)
-client.models.delete("ft:gpt-3.5-turbo:acemeco:suffix:abc123")
+  # Delete a fine-tuned model (must be an owner of the org the model was created in)
+  client.models.delete("ft:gpt-3.5-turbo:acemeco:suffix:abc123")
 
 
 response = client.chat.completions.create(
